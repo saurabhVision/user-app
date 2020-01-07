2d056eb1cc11222ea22bd269b99e7aa9281fad24
LeoDubey <saurabh.dubey97666@gmail.com>
2020-01-08 01:39:34 +0530

    userdemo release0.1 commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO 
 	USERINFO (first_name, last_name, email,pincode,birthdate)
 VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com',600100,'2001-01-01'),
-  	('John', 'Doe', 'xyz@email.com',600101,'2002-02-02');
+  	('test1', 'lname', 'test1@gmail.com',600100,'2001-01-01'),
+  	('test2', 'lname2', 'xyz@email.com',600101,'2002-02-02');
